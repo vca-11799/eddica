a4cb5ebddc721774b8e438844b221e3d50ab1660
vca-11799 <vishalagravat555@gmail.com>
2025-03-08 09:53:19 +0530

    unnecessary controller removed.

--- eddica_project/routes/web.php
+++ eddica_project/routes/web.php
@@ -8,4 +8,3 @@ use App\Http\Controllers\dashboard\dashboardController;
 Route::get('/', [dashboardController::class, 'dashboardIndex'])->name('dashboard.index');
 Route::get('/bodycompoctionMaster', [bodyCampoctionController::class, 'bodyCompoctionMaster'])->name('body.compoction.master');
 Route::get('/addbodycompoction', [bodyCampoctionController::class, 'addbodyCompoction'])->name('add.body.compoction');
-Route::post('/dashboardController', [maincontroller::class, 'maincontroller'])->name('maincontroller');
